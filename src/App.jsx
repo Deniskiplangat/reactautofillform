66f05cec67ebeb16f1d9f200a33cbdce955b4ab3
Deniskiplangat <denisruto3561@gmail.com>
2025-02-17 16:51:32 +0300

    added handle submit function

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ function App() {
   const [barcode,setBarcode] = useState('')
   const [supplier,setSupplier] = useState('')
   const [description,setDescription] = useState('')
+  const [quantity, setQuantity] = useState('');
+  const [expiryDate, setExpiryDate] = useState('');
   const [error, setError] = useState('');
 
 
@@ -35,10 +37,29 @@ function App() {
       }
     }
   }
+
+  //handle submitting of the products
+  const handleSubmit = async(event)=>{
+      event.preventDefault()
+      try {
+        const response = await axios.post(`http://localhost:4000/expiries`,{
+          quantity,
+          product_id: barcode, // Assuming barcode is used as product_id
+          expiry_date: expiryDate,
+        })
+        console.log(response.data);
+        setQuantity('');
+        setExpiryDate('');
+        setError('');
+      } catch (error) {
+        console.error('Error posting expiry data:', error);
+        setError('Error posting expiry data');
+      }
+  }
   return (
     <>
       <div className="flex flex-col justify-around items-center container w-xl h-96 border-2 border-sky-500">
-        <form action="">
+        <form onSubmit={handleSubmit}>
         <div className="w-auto h-72 flex flex-col justify-around items-center">
           <div>
             <input
@@ -72,17 +93,22 @@ function App() {
           <div>
             <input type="number" 
             placeholder="quantity" 
-            className="input" />
+            className="input"
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value)}
+            />
           </div>
           <div>
             <input
               type="date"
               placeholder="expiry date"
               className="input w-48"
+              value={expiryDate}
+              onChange={(e) => setExpiryDate(e.target.value)}
             />
           </div>
           <div className="flex justify-center items-center">
-            <button className="btn btn-success">Submit expiry</button>
+            <button className="btn btn-success" type="submit">Submit expiry</button>
           </div>
         </div>
         </form>
